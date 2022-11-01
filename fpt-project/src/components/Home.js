2e015a8ff2a7ac88f07316cdbe158c66a3ac1997
Minh Hieu <vmhiesu@gmail.com>
2022-11-01 21:13:10 +0700

    home screen

---
+++ fpt-project/src/components/Home.js
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import '../style/home.css';
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <div className='columns' style={{display: "flex", justifyContent: "space-between"}}>
+                <div className='fpt-logo'>
+                    <img src='fpt-logo.png'></img>
+                </div>
+                <div className='login'>
+                    <button className='button-login'>
+                        Login
+                    </button>
+                </div>
+            </div>
+            <hr style={{margin: 0, color: 'gray'}}/>
+            <div className='home-container' style={{display: "flex", justifyContent: "space-evenly"}}> 
+                <div className='title'>
+                    <h1 >Social Constructive Learning</h1>
+                    <h3 className='content'>Construct knowledge and personalize the learning way to empower learners' full potential.</h3>
+                    <button className='button-join'>
+                        Join now
+                    </button>
+                </div>
+                <div>
+                    <img src='education.png'></img>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;

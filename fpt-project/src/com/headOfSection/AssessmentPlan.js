87fc8767ec256cf90f9296e11ba892ab1a20e489
Viet Khanh <dkhanh292000@gmail.com>
2022-11-28 11:25:14 +0700

    update

--- fpt-project/src/com/headOfSection/AssessmentPlan.js
+++ fpt-project/src/com/headOfSection/AssessmentPlan.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Tabs } from 'antd';
 import AssessmentPlanDetail from './AssessmentPlanDetail';
+import { apiClient } from '../../request-api/api_client';
 
 const AssessmentPlan = () => {
+    const [listData, setListData] = useState([]);
     const location = useLocation()
+    const id = window.location.pathname.split("/")[2];
     const onChange = (key) => {
         console.log(key);
-      };
-
+    };
+    const _requestData = async () => {
+        const { data } = await apiClient.get(`/api/result-observation-slot?oSlotId=${id}`)
+        setListData(data.items)
+    }
+    useEffect(() => {
+        _requestData()
+    }, [])
     return (
         <div style={{ padding: 24 }}>
-            <div style={{ fontSize: 28, fontWeight: 500 , marginBottom : 32 }}>Thông tin nhận :</div>
+            <div style={{ fontSize: 28, fontWeight: 500, marginBottom: 32 }}>Thông tin nhận :</div>
             <Tabs
                 onChange={onChange}
                 type="card"
-                size = 'large'
+                size='large'
                 items={new Array(3).fill(null).map((_, i) => {
                     const id = String(i + 1);
                     return {
                         label: `Đánh giá ${id}`,
                         key: id,
-                        children: <AssessmentPlanDetail id={id}/>,
+                        children: <AssessmentPlanDetail id={id} item={listData[i] || {}} />,
                     };
                 })}
             />
--- fpt-project/src/com/headOfSection/AssessmentPlanDetail.js
+++ fpt-project/src/com/headOfSection/AssessmentPlanDetail.js
@@ -1,53 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Table } from 'antd';
 
 const AssessmentPlanDetail = (props) => {
-    const dataSource = [
-        {
-            key: '1',
-            name: 'Mike',
-            age: 32,
-            address: '10 Downing Street',
-        },
-        {
-            key: '2',
-            name: 'John',
-            age: 42,
-            address: '10 Downing Street',
-        },
-    ];
+    console.log(props.item);
+    const [dataSource , setDataSource] = useState([])
     const columns = [
         {
-            title: 'Name',
+            title: 'Tên tiêu chí',
             dataIndex: 'name',
             key: 'name',
+            render : (text, record) => {
+                if(record.sumPoint){
+                    return <p style={{color : 'green' , fontWeight : 500}}>{text}</p>
+                }
+                else
+                {
+                    return <p>{text}</p>
+                }
+            },
         },
         {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
-        },
-        {
-            title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
+            title: 'Điểm',
+            dataIndex: 'point',
+            key: 'point',
         },
     ];
+    useEffect(() => {
+        if(props.item?.listOfObservationDetail && props.item.listOfObservationDetail.length > 0){
+            const sumPoint = props.item.listOfObservationDetail.reduce((prv , cur) => {
+                return prv + cur.point
+            } , 0)            
+            setDataSource([
+                ...props.item.listOfObservationDetail,
+                {
+                    id : 'a',
+                    name : "Tổng điểm",
+                    point : sumPoint,
+                    sumPoint : true
+                }
+            ])
+        }
+    } ,[])
     return (
         <div>
             <div style={{ display: 'flex', fontSize: 20 }}>
-                <div style={{ width: 300 }}>Bạn có tham gia không:</div>
-                <div>Có</div>
+                <div style={{ width: 200 }}>Điểm tốt:</div>
+    <div>{props.item?.advantage || ''}</div>
             </div>
             <div style={{ display: 'flex', fontSize: 20 }}>
-                <div style={{ width: 300 }}>Người đánh giá ứng viên:</div>
-                <div>khanh-khanh123@gmail.com</div>
+                <div style={{ width: 200 }}>Bình luận:</div>
+    <div>{props.item?.comment || ''}</div>
+            </div>
+            <div style={{ display: 'flex', fontSize: 20 }}>
+                <div style={{ width: 200 }}>Điểm xấu:</div>
+                <div>{props.item?.disadvantage || ''}</div>
             </div>
             <div style={{ fontSize: 20, color: 'blue' }}>
                 Bảng đánh gía chi tiết dạy giáo dục :
             </div>
             <div style={{width : '50%'}}>
-                <Table dataSource={dataSource} columns={columns} pagination={false} />;
+                <Table dataSource={dataSource} columns={columns} pagination={false} />
             </div>
         </div>
     );

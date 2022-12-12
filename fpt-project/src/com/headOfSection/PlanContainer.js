37bbd94465bcd705860d26b044da3d8afe4cfb31
Viet Khanh <dkhanh292000@gmail.com>
2022-12-12 17:43:27 +0700

    fix bug

--- fpt-project/src/com/headOfSection/PlanContainer.js
+++ fpt-project/src/com/headOfSection/PlanContainer.js
@@ -7,8 +7,33 @@ import Header from '../Header';
 import { useNavigate } from 'react-router-dom';
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 import { openNotificationWithIcon } from '../../request/notification';
+import { CardCustom } from '../../helper/styled_component';
+import {
+  PlusOutlined, DeleteOutlined, FilterOutlined, ReloadOutlined,
+  UploadOutlined, UnorderedListOutlined
+} from "@ant-design/icons";
 
 
+const dataStatus = [
+  {
+    name : 'Đợi duyệt',
+    backgroundColor : 'yellow',
+    color : 'black',
+    disabled : false
+  },
+  {
+    name : 'Đã duyệt',
+    backgroundColor : '#1677ff',
+    color : 'black',
+    disabled : true
+  },
+  {
+    name : 'Từ chối',
+    backgroundColor : '#ff4d4f',
+    color : 'black',
+    disabled : false
+  }
+]
 
 const PlanContainer = () => {
   const navigation = useNavigate();
@@ -25,62 +50,74 @@ const PlanContainer = () => {
   const [room, setRoom] = useState([]);
   const [subject, setSubject] = useState([]);
   const [accounts, setAccounts] = useState([]);
+  const [selectedRow, setSelectRow] = useState([]);
+  const [status , setStatus] = useState({});
+
+
   const campusId = localStorage.getItem('campusId');
 
+  const _getStatusListPlan = async(id) => {
+    const { data } = await apiClient.get(`/api/status-observation-plan?planId=${id}`)
+    console.log("daaaa" ,data);
+    setStatus(dataStatus[data.items])
+  }
+
   const _requestData = async () => {
-    const {data} = await apiClient.get(`/api/list-observation-slot?semesterId=${semesterId}&accountId=${userId}`)
-     data.items = data.items.map((item, idx) => {
+    const { data } = await apiClient.get(`/api/list-observation-slot?semesterId=${semesterId}&accountId=${userId}`)
+    _getStatusListPlan(data.items[0].id)
+    data.items = data.items.map((item, idx) => {
       var date = new Date(`${item.slotTime}`);
-        item.slotTime =
+      item.slotTime =
         ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear();
-        let accountName1 = accounts.find(o => o.value == item.accountId1)?.name;
-        var accountName2 = accounts.find(o => o.value == item.accountId2)?.name;
-        var roomName = room.find(o => o.value == item.roomId)?.name;
-        var subjectCode = subject.find(o => o.value == item.subjectId)?.name;
-        var slotName = slot.find(o => o.value == item.slotId)?.name;
-        return {...item, accountName1: accountName1, accountName2: accountName2, roomName: roomName, subjectCode: subjectCode, slotName: slotName};
+      let accountName1 = accounts.find(o => o.value == item.accountId1)?.name;
+      var accountName2 = accounts.find(o => o.value == item.accountId2)?.name;
+      var roomName = room.find(o => o.value == item.roomId)?.name;
+      var subjectCode = subject.find(o => o.value == item.subjectId)?.name;
+      var slotName = slot.find(o => o.value == item.slotId)?.name;
+      return { ...item, accountName1: accountName1, accountName2: accountName2, roomName: roomName, subjectCode: subjectCode, slotName: slotName , key : item.id };
     })
     setListPlan(data.items);
   }
   const getSlot = async () => {
-    const {data} = await apiClient.get('/api/slot-list')
+    const { data } = await apiClient.get('/api/slot-list')
     var rooms = data;
     rooms = rooms.map((item, idx) => {
-      return {...item, label: item.name}
+      return { ...item, label: item.name }
     })
     setSlot(rooms);
   }
   const getSubjects = async () => {
-    const {data} = await apiClient.get(`/api/subject-dropdown-list?id=${campusId}&code=`)
+    const { data } = await apiClient.get(`/api/subject-dropdown-list?id=${campusId}&code=`)
     var subjects = data;
     subjects = subjects.map((item, idx) => {
-      return {...item, label: item.name}
+      return { ...item, label: item.name }
     })
     setSubject(subjects);
   }
   const getRooms = async () => {
-    const {data} = await apiClient.get(`/api/room-dropdown-list?id=${campusId}&name=`)
+    const { data } = await apiClient.get(`/api/room-dropdown-list?id=${campusId}&name=`)
     var rooms = data;
     rooms = rooms.map((item, idx) => {
-      return {...item, label: item.name}
+      return { ...item, label: item.name }
     })
     setRoom(rooms);
   }
   const getAccounts = async () => {
-    const {data} = await apiClient.get(`/api/list-account?id=${campusId}&email=`)
+    const { data } = await apiClient.get(`/api/list-account?id=${campusId}&email=`)
     var rooms = data;
     rooms = rooms.map((item, idx) => {
-      return {...item, label: item.name}
+      return { ...item, label: item.name }
     })
     setAccounts(rooms);
     setIsDoneAccount(true)
   }
 
   const getSemesters = async () => {
-    const {data} = await apiClient.get('/api/semester-list')
+    const { data } = await apiClient.get('/api/semester-list')
+    setSemesterId(data.length)
     setListSemesters(data);
   }
-  
+
   useEffect(() => {
     getSemesters();
     getSlot();
@@ -89,9 +126,9 @@ const PlanContainer = () => {
     getAccounts();
     setIsDone(true)
   }, [])
-  
+
   useEffect(() => {
-    if(isDone & isDoneAccount){
+    if (isDone & isDoneAccount) {
       _requestData();
       setCount(semesterId)
     }
@@ -126,22 +163,22 @@ const PlanContainer = () => {
       dataIndex: 'slotName',
       key: 'slotName',
       render: (text, record, idx) => (
-        isUpdate && idx == index ? 
-        <Select className='select-box' style={{width: '100%'}} defaultValue={text} options={slot} onChange={(e) => handleSlotChange(e, record)}/>
-        : <div>{text}</div>
-       ),
-       width: '10%'
+        isUpdate && idx == index ?
+          <Select className='select-box' style={{ width: '100%' }} defaultValue={text} options={slot} onChange={(e) => handleSlotChange(e, record)} />
+          : <div>{text}</div>
+      ),
+      width: '10%'
     },
     {
       title: 'Phòng học',
       dataIndex: 'roomName',
       key: 'roomName',
       render: (text, record, idx) => (
-        isUpdate && idx == index ? 
-        <Select className='select-box' style={{width: '100%'}} defaultValue={text} options={room} onChange={(e) => handleRoomChange(e, record)}/>
-        : <div>{text}</div>
-       ),
-       width: '10%'
+        isUpdate && idx == index ?
+          <Select className='select-box' style={{ width: '100%' }} defaultValue={text} options={room} onChange={(e) => handleRoomChange(e, record)} />
+          : <div>{text}</div>
+      ),
+      width: '10%'
 
     },
     {
@@ -149,11 +186,11 @@ const PlanContainer = () => {
       dataIndex: 'subjectCode',
       key: 'subjectCode',
       render: (text, record, idx) => (
-        isUpdate && idx == index ? 
-        <Select className='select-box' style={{width: '100%'}} defaultValue={text} options={subject} onChange={(e) => handleSubjectChange(e, record)}/>
-        : <div>{text}</div>
-       ),
-       width: '10%'
+        isUpdate && idx == index ?
+          <Select className='select-box' style={{ width: '100%' }} defaultValue={text} options={subject} onChange={(e) => handleSubjectChange(e, record)} />
+          : <div>{text}</div>
+      ),
+      width: '10%'
 
     },
     {
@@ -186,38 +223,55 @@ const PlanContainer = () => {
       key: 'result',
       dataIndex: 'result',
       render: (text, record) => (
-       <Button onClick={() => handleNavigation(record)}>
-         {"Kết quả"}
-       </Button>
+        <Button onClick={() => handleNavigation(record)}>
+          {"Kết quả"}
+        </Button>
       ),
     },
     {
       title: 'Cập nhật',
       render: (text, record, idx) => (
-        isUpdate && idx == index ? 
+        isUpdate && idx == index ?
           <Button type="primary" variant="outlined" onClick={handleClickOpen}>
             Xác nhận
           </Button>
-        :
+          :
           <Button type="primary" onClick={() => updateSlot(record, idx)}>
-          {"Cập nhật"}
+            {"Cập nhật"}
           </Button>
       ),
     },
-    
+
   ];
+
+  const _handleDel = () => {
+    const isBool = window.confirm("Bạn có muốn xoá không")
+    if (isBool) {
+        selectedRow.map(async (item) => {
+            try {
+                const { data } = await apiClient.post(`/api/delete-plan?id=${item}`)
+                openNotificationWithIcon("success", "Xoá thành công");
+            } catch (error) {
+                openNotificationWithIcon("error",error.message)
+            }
+        })
+    }
+    setTimeout(() => {
+      _requestData()
+    } ,1000)
+}
 
   const [result, setResult] = useState({});
   const handleRoomChange = (e, record) => {
-    var values = {...record, roomId: e}
+    var values = { ...record, roomId: e }
     setResult(values);
   }
   const handleSlotChange = (e, record) => {
-    var values = {...record, slotId: e}
+    var values = { ...record, slotId: e }
     setResult(values);
   }
   const handleSubjectChange = (e, record) => {
-    var values = {...record, subjectId: e}
+    var values = { ...record, subjectId: e }
     setResult(values);
   }
   const [isUpdate, setIsUpdate] = useState(false);
@@ -231,20 +285,20 @@ const PlanContainer = () => {
     var values = {
       id: result.id,
       accountId: result.accountId,
-      subjectId:result.subjectId,
-      reason:result.reason,
-      slotTime:result.slotTime.split("/").reverse().join("-"),
-      slotId:result.slotId,
-      roomId:result.roomId,
-      className:result.className,
+      subjectId: result.subjectId,
+      reason: result.reason,
+      slotTime: result.slotTime.split("/").reverse().join("-"),
+      slotId: result.slotId,
+      roomId: result.roomId,
+      className: result.className,
       headTraining: parseInt(userId),
       headSubject: parseInt(userId),
-      accountId1:result.accountId1,
-      accountId2:result.accountId2,
+      accountId1: result.accountId1,
+      accountId2: result.accountId2,
     };
-    const {data} = await apiClient.post('/api/update-observation-slot' , values)
+    const { data } = await apiClient.post('/api/update-observation-slot', values)
     setDialogOpen(false);
-    if(data.status == 200){
+    if (data.status == 200) {
       openNotificationWithIcon("success", "Cập nhật thành công")
       setIndex(-1);
       _requestData();
@@ -259,10 +313,10 @@ const PlanContainer = () => {
       dataIndex: 'totalPoint',
       key: 'totalPoint',
       render: (text, record) => (
-        <Button style={{width : 130}} onClick={() => setSemesterId(record.value)} className='is-clickable' >
+        <Button style={{ width: 130 }} onClick={() => setSemesterId(record.value)} className='is-clickable' >
           {record.name}
         </Button>
-       ),
+      ),
     },
   ]
 
@@ -277,57 +331,89 @@ const PlanContainer = () => {
   const handleClose = () => {
     setDialogOpen(false);
   };
-  
+
   return (
     <>
-    <Header />
-    <div className='plan-container'>
-      <div className='modal-plan'>
-        <Button type="primary"  disabled={count ==listSemesters?.length -1 ? false : true} onClick={showModal}>
-          Tạo kế hoạch dự giờ
+      <Header />
+      <div className='plan-container'>
+        <div className='modal-plan'>
+          <Button type="primary" disabled={semesterId == listSemesters?.length ? false : true} onClick={showModal}>
+            Tạo kế hoạch dự giờ
         </Button>
-        <Drawer
-          width={820}
-          open={open}
-          title={
-            <div className='has-text-weight-bold is-size-4'>
-              Tạo kế hoạch dự giờ
+          <Drawer
+            width={820}
+            open={open}
+            title={
+              <div className='has-text-weight-bold is-size-4'>
+                Tạo kế hoạch dự giờ
             </div>
-          }
-          onOk={handleOk}
-          onClose={handleCancel}
-          footer={null}
+            }
+            onOk={handleOk}
+            onClose={handleCancel}
+            footer={null}
           >
-          <ModalPlanContainer handleCancel={handleCancel}/>
-        </Drawer>
-      </div>
-      <Dialog
-            open={dialogOpen}
-            onClose={handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-          >
-            <DialogTitle id="alert-dialog-title">
-              {"Cập nhật thông tin"}
-            </DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-                Cập nhật
+            <ModalPlanContainer handleCancel={handleCancel} />
+          </Drawer>
+        </div>
+        <Dialog
+          open={dialogOpen}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Cập nhật thông tin"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Cập nhật
               </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose}>Huỷ bỏ</Button>
-              <Button onClick={postUpdateSlot} autoFocus>
-                Đồng ý
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Huỷ bỏ</Button>
+            <Button onClick={postUpdateSlot} autoFocus>
+              Đồng ý
               </Button>
-            </DialogActions>
-          </Dialog>
-      <div className='columns'>
+          </DialogActions>
+        </Dialog>
+        <div className='columns'>
           <div className='column ml-4 is-1 mr-6'>
-              {listSemesters?.length > 0 && <Table columns={semesterColums} dataSource={listSemesters} pagination={false}/>}
+            {listSemesters?.length > 0 &&
+              <Table
+                columns={semesterColums}
+                dataSource={listSemesters}
+                pagination={false}
+              />}
           </div>
-          <div className='column' style={{borderLeft: "1px solid black"}}>
-              {listPlan?.length > 0 && <Table columns={columns} dataSource={listPlan} />}
+          <div className='column' style={{ borderLeft: "1px solid black" }}>
+            {listPlan?.length > 0 &&
+              <CardCustom
+                title={
+                  <div style={{display :'flex' , alignContent : 'center'}}>
+                    <p style={{marginRight : 20}}>Bảng trưởng bộ môn</p>
+                <Button onClick={() => postUpdateSlot()} disabled={status.disabled} style={{background : status?.backgroundColor || 'blue' , color : status.color}}>{status?.name || ''}</Button>
+                  </div>
+                }
+                extra={<Extra
+                  showDel={selectedRow && selectedRow[0]}
+                  // listColumn={[]}
+
+                  _onReload={_requestData}
+                  _handleDel={selectedRow.length > 0 ? _handleDel : () => { }}
+                  // _onClickAdd={() => setShowAddNew(true)}
+              // _onClickColumnShow={() => setShowColumn(true)}
+              />}
+              >
+                <Table
+                  rowSelection={{
+                    type: 'checkbox',
+                    onChange: (selectedRowKeys, selectedRows) => {
+                      console.log(selectedRowKeys, selectedRows);
+                      setSelectRow(selectedRowKeys)
+                    }
+                  }}
+                  columns={columns}
+                  dataSource={listPlan} /></CardCustom>}
           </div>
         </div>
       </div>
@@ -338,4 +424,26 @@ export default PlanContainer;
 
 
 
+const Extra = ({
+  showDel = true,
+
+  _handleDel = () => { },
+  _onClickAdd = () => { },
+  _onFilter = () => { },
+  _onReload = () => { },
+  // _onClickColumnShow = () => { },
+}) => {
+
+  return (
+      <div style={{ display: 'flex', alignItems: 'center', paddingRight: 7, justifyContent: 'space-between' }}>
+          <div style={{ display: 'flex', flex: 1 }}>
+              <div style={{ display: 'flex' }}>
+                  {!showDel ? null : <Button onClick={_handleDel} className="ro-custom" type="text" icon={<DeleteOutlined />} >Xoá item đã chọn</Button>}
+                  <Button onClick={() => _onReload()} className="ro-custom" type="text" icon={<ReloadOutlined />} >Làm mới</Button>
+                  {/* <Button onClick={_onClickAdd} className="ro-custom" type="text" icon={<PlusOutlined />} >Thêm</Button> */}
+              </div>
+          </div>
+      </div>
+  )
+}
 
